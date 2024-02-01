74468711500755d377279a59184f952fa00e91b0
Eman Gurung <egurung1117@gmail.com>
2024-02-01 04:23:00 -0500

    completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,14 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let seen = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (seen.includes(arr[i])) {
+      return arr[i]
+    }
+    seen.push(arr[i])
+  }
+
+  return -1
 }
 
 if (require.main === module) {
